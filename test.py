e44b26c52ff310ef7898834f4af938fdeb5c1905
Cedric Honnet <honnet@telecom-paristech.org>
2018-08-01 23:28:27 +0200

    Add python test file.

---
+++ test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import glob
+import serial
+import platform
+
+
+###############################################################################
+# TODO: implement an interactive terminal with getch()?
+# https://stackoverflow.com/questions/510357/python-read-a-single-character-from-the-user
+
+def main():
+    port = serial_init()
+    d = 100
+
+    while True:
+        go_to(0, d, port)
+        go_to(d, d, port)
+        go_to(d, 0, port)
+        go_to(0, 0, port)
+
+
+###############################################################################
+def serial_init():
+    PLATFORM = platform.system()
+    if "Linux" in PLATFORM:
+        SERIAL_PATH = "/dev/ttyUSB*"
+    elif "Darwin" in PLATFORM:
+        SERIAL_PATH = "/dev/tty.usb*"
+    else: # Windows
+        port = serial.Serial('COM5', 115200)        # TODO ADAPT COM NUMBER !!
+        SERIAL_PATH = 'WIN_WORKARAOUND'
+
+    if SERIAL_PATH != 'WIN_WORKARAOUND':
+        devices = glob.glob(SERIAL_PATH)
+        port = serial.Serial(devices[0], 115200)
+
+    success = port.isOpen()
+
+    if success:
+        print("Port open, waiting for calibration...\n")
+        line = port.readline()
+        while ("start".encode(encoding="utf-8") not in line):
+            line = port.readline()
+    else:
+        print("\n!!! Error: serial device not found !!!")
+        exit(-1)
+    return port
+
+
+###############################################################################
+def go_to(x, y, port):
+    command = "G X" + str(x) + " Y" + str(y) + "\n"
+    port.write(command.encode(encoding="utf-8"))
+    print(command)
+
+    # wait for acknowledgment
+    line = port.readline()
+    while ("ok".encode(encoding="utf-8") not in line):
+        line = port.readline()
+
+
+###############################################################################
+if __name__ == "__main__":
+    main()
+

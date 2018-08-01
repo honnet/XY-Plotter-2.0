c0628d0e4a893bb54875a427ccdab149e1dd1e9c
Cedric Honnet <honnet@telecom-paristech.org>
2018-08-01 20:15:51 +0200

    Remove trailing whitespaces.

--- software/GCodeParser/GCodeParser.ino
+++ software/GCodeParser/GCodeParser.ino
@@ -64,75 +64,75 @@ int targetPosServo = 90;
 bool comment = false;
 void setup()
 {
-	//Do startup stuff here
-	Serial.begin(115200);
-        if(Z_ENABLE_SERVO==1){
-          servo.attach(Z_STEP_PIN);
-        }
-	//other initialization.
-	init_process_string();
-	init_steppers();
-	process_string("G90",3);//Absolute Position
-        Serial.println("start");
+    //Do startup stuff here
+    Serial.begin(115200);
+    if(Z_ENABLE_SERVO==1){
+        servo.attach(Z_STEP_PIN);
+    }
+    //other initialization.
+    init_process_string();
+    init_steppers();
+    process_string("G90",3);//Absolute Position
+    Serial.println("start");
 }
 
 void loop()
 {
-  
-	char c;
-	//read in characters if we got them.
-	if (Serial.available() > 0)
-	{
-		c = Serial.read();
-		no_data = 0;
-		//newlines are ends of commands.
-		if (c != '\n')
-		{
-			if(c==0x18){
-				Serial.println("Grbl 1.0");
-			}else{
-                          if (c == '('){
-                            comment = true;
-                          }
-                          // If we're not in comment mode, add it to our array.
-                          if (!comment)
-                          {
-                            commands[serial_count] = c;
-                    				serial_count++;
-                          }
-                          if (c == ')'){
-                            comment = false; // End of comment - start listening again
-                          }
-                        }
-				
-		}
-	}else
-	{
-		no_data++;
-		delayMicroseconds(100);
 
-	//if theres a pause or we got a real command, do it
-	if (serial_count && (c == '\n' || no_data > 100))
-	{
-		//process our command!
-		process_string(commands, serial_count);
-		//clear command.
-		init_process_string();
-	}
+    char c;
+    //read in characters if we got them.
+    if (Serial.available() > 0)
+    {
+        c = Serial.read();
+        no_data = 0;
+        //newlines are ends of commands.
+        if (c != '\n')
+        {
+            if(c==0x18){
+                Serial.println("Grbl 1.0");
+            }else{
+                if (c == '('){
+                    comment = true;
+                }
+                // If we're not in comment mode, add it to our array.
+                if (!comment)
+                {
+                    commands[serial_count] = c;
+                    serial_count++;
+                }
+                if (c == ')'){
+                    comment = false; // End of comment - start listening again
+                }
+            }
 
-	//no data?  turn off steppers
-	if (no_data > 1000){
-		disable_steppers();
-	}
         }
-//        return;
-//                delay(5);
-//                int dPos = abs(currentPosServo-targetPosServo);
-//                if(currentPosServo<targetPosServo){
-//                   currentPosServo += dPos>8?6:1;
-//                }else if(currentPosServo>targetPosServo){
-//                   currentPosServo -= dPos>8?6:1;
-//                }
-                
-        
+    }else
+    {
+        no_data++;
+        delayMicroseconds(100);
+
+        //if theres a pause or we got a real command, do it
+        if (serial_count && (c == '\n' || no_data > 100))
+        {
+            //process our command!
+            process_string(commands, serial_count);
+            //clear command.
+            init_process_string();
+        }
+
+        //no data?  turn off steppers
+        if (no_data > 1000){
+            disable_steppers();
+        }
+    }
+    //        return;
+    //                delay(5);
+    //                int dPos = abs(currentPosServo-targetPosServo);
+    //                if(currentPosServo<targetPosServo){
+    //                   currentPosServo += dPos>8?6:1;
+    //                }else if(currentPosServo>targetPosServo){
+    //                   currentPosServo -= dPos>8?6:1;
+    //                }
+
+
 }
